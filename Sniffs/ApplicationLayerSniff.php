8c30e90020995d052cbb7579cd860d040db91749
Gavin Staniforth <gavin@usemarkup.com>
2018-02-14 12:56:00 +0000

    feat: add next specific sniffs for layers

---
+++ Sniffs/ApplicationLayerSniff.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Markup\Sniffs;
+
+use SlevomatCodingStandard\Helpers\UseStatement;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+class ApplicationLayerSniff implements \PHP_CodeSniffer\Sniffs\Sniff
+{
+    private const APPLICATION_LAYER_PATH = 'src/Next/Application/';
+
+    private const PROHIBITED_NAMESPACES = [
+        'Doctrine\\Orm',
+    ];
+
+    /**
+     * @return mixed[]
+     */
+    public function register()
+    {
+        return [
+            T_OPEN_TAG,
+        ];
+    }
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile
+     * @param int $openTagPointer
+     */
+    public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $openTagPointer)
+    {
+        if (stripos($phpcsFile->getFilename(), self::APPLICATION_LAYER_PATH) === false) {
+            return;
+        }
+
+        $useStatements = UseStatementHelper::getUseStatements($phpcsFile, $openTagPointer);
+
+        /** @var UseStatement $useStatement */
+        foreach ($useStatements as $useStatement) {
+            foreach (self::PROHIBITED_NAMESPACES as $namespace) {
+                if (stripos($useStatement->getFullyQualifiedTypeName(), $namespace) !== false) {
+                    $phpcsFile->addError(
+                        sprintf(
+                            'Application code cannot reference %s namespace',
+                            $useStatement->getFullyQualifiedTypeName()
+                        ),
+                        $openTagPointer,
+                        __CLASS__
+                    );
+                }
+            }
+        }
+    }
+
+}
---
+++ Sniffs/DomainLayerSniff.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace Markup\Sniffs;
+
+use SlevomatCodingStandard\Helpers\UseStatement;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+class DomainLayerSniff implements \PHP_CodeSniffer\Sniffs\Sniff
+{
+    private const DOMAIN_LAYER_PATH = 'src/Next/Domain/';
+
+    private const WHITELIST_NAMESPACES = [
+        'Next\\Domain\\Common',
+
+        # Doctrine Common Collections is allowed in the namespace
+        'Doctrine\Common\Collections',
+    ];
+
+    /**
+     * @return mixed[]
+     */
+    public function register()
+    {
+        return [
+            T_OPEN_TAG
+        ];
+    }
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile
+     * @param int $openTagPointer
+     */
+    public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $openTagPointer)
+    {
+        if (stripos($phpcsFile->getFilename(), self::DOMAIN_LAYER_PATH) === false) {
+            return;
+        }
+
+        $currentDomain = null;
+
+        if (preg_match(sprintf('/%s(.*?)\/./i', addcslashes(self::DOMAIN_LAYER_PATH, '/')), $phpcsFile->getFilename(), $matches) !== false) {
+            if (!empty($matches)) {
+                $currentDomain = end($matches);
+            }
+        }
+
+        if ($currentDomain == null) {
+            return;
+        }
+
+        $useStatements = UseStatementHelper::getUseStatements($phpcsFile, $openTagPointer);
+
+        $whitelist = self::WHITELIST_NAMESPACES;
+        $whitelist[] = $currentDomain;
+
+        /** @var UseStatement $useStatement */
+        foreach ($useStatements as $useStatement) {
+            $found = false;
+
+            foreach ($whitelist as $namespace) {
+                if (stripos($useStatement->getFullyQualifiedTypeName(), $namespace) !== false) {
+                    $found = true;
+                }
+            }
+
+            if (!$found) {
+                $statement = $useStatement->getFullyQualifiedTypeName();
+
+                // If its a NATIVE php class/interface its ok
+                if (class_exists($statement, false) || interface_exists($statement, false)) {
+                    return;
+                }
+
+                $phpcsFile->addError(
+                    sprintf(
+                        'Domain code cannot reference %s as its outside of the domain, seek advice on this matter.',
+                        $useStatement->getFullyQualifiedTypeName()
+                    ),
+                    $openTagPointer,
+                    __CLASS__
+                );
+            }
+        }
+    }
+
+}
