2c70eea5bc132c61903cdef00a8b2c4459138f03
Gavin Staniforth <gavin@usemarkup.com>
2018-02-28 22:12:00 +0000

    feat: prevent the application exposing the domain

---
+++ Markup/Sniffs/Next/PreventExposingDomainSniff.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace Markup\Sniffs\Next;
+
+use SlevomatCodingStandard\Helpers\ClassHelper;
+use SlevomatCodingStandard\Helpers\FunctionHelper;
+use SlevomatCodingStandard\Helpers\ReturnTypeHint;
+use SlevomatCodingStandard\Helpers\UseStatement;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+class PreventExposingDomainSniff implements \PHP_CodeSniffer\Sniffs\Sniff
+{
+    private $useStatements = [];
+
+    /**
+     * @return mixed[]
+     */
+    public function register()
+    {
+        return [
+            T_OPEN_TAG,
+            T_FUNCTION,
+        ];
+    }
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile
+     * @param int $openTagPointer
+     */
+    public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $openTagPointer)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $token = $tokens[$openTagPointer];
+        $filename = $phpcsFile->getFilename();
+
+        if ($token['type'] == 'T_OPEN_TAG') {
+            $this->useStatements[$filename] = UseStatementHelper::getUseStatements(
+                $phpcsFile,
+                $openTagPointer
+            );
+
+            return;
+        }
+
+
+        /** @var ReturnTypeHint $type */
+        $type = FunctionHelper::findReturnTypeHint($phpcsFile, $openTagPointer);
+
+        if ($type === null) {
+            return;
+        }
+
+        try {
+            $class = ClassHelper::getFullyQualifiedName($phpcsFile, $openTagPointer);
+        } catch (\Throwable $e) {
+            return;
+        }
+
+        /** @var UseStatement $useStatement */
+        foreach ($this->useStatements[$filename] as $useStatement) {
+            if (stripos($useStatement->getFullyQualifiedTypeName(), $type->getTypeHint()) !== false) {
+                if (stripos($useStatement->getFullyQualifiedTypeName(), 'Next\Domain\\') !== false) {
+                    $phpcsFile->addError(
+                        sprintf(
+                            'Application code %s should not be exposing domain layer code %s',
+                            $class,
+                            $useStatement->getFullyQualifiedTypeName()
+                        ),
+                        $openTagPointer,
+                        'DomainBeingExposed'
+                    );
+                }
+            }
+        }
+    }
+}
