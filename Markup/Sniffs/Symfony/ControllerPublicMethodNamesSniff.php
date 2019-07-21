c0b0975e12b27afe164c8bea9fd5b9450ad579fb
Chris D Brown <owner@chrisdbrown.co.uk>
2019-07-21 16:39:06 +0100

    feat: Only allow actions as public controller functions

---
+++ Markup/Sniffs/Symfony/ControllerPublicMethodNamesSniff.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+namespace Markup\Sniffs\Symfony;
+
+use SlevomatCodingStandard\Helpers\FunctionHelper;
+
+class ControllerPublicMethodNamesSniff implements \PHP_CodeSniffer\Sniffs\Sniff
+{
+    private const CONTROLLER_NAMESPACE = '/Controller/';
+    private const CONTROLLER_FILE_SUFFIX = 'Controller.php';
+    /**
+     * @return mixed[]
+     */
+    public function register()
+    {
+        return [
+            T_FUNCTION,
+        ];
+    }
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile
+     * @param int $openTagPointer
+     */
+    public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $openTagPointer)
+    {
+        if (stripos($phpcsFile->getFilename(), self::CONTROLLER_NAMESPACE) === false) {
+            return;
+        }
+
+        if (stripos($phpcsFile->getFilename(), self::CONTROLLER_FILE_SUFFIX) === false) {
+            return;
+        }
+
+        if (!FunctionHelper::isMethod($phpcsFile, $openTagPointer)) {
+            return;
+        }
+
+        $name = FunctionHelper::getName($phpcsFile, $openTagPointer);
+
+        try {
+            $properties = $phpcsFile->getMethodProperties($openTagPointer);
+
+            if (!isset($properties['scope']) || $properties['scope'] !== 'public') {
+                return;
+            }
+        } catch (\Throwable $e) {
+            return;
+        }
+
+        if (in_array($name, ['__construct', '__invoke'])) {
+            return;
+        }
+
+        if (substr($name, -6) !== 'Action') {
+            $phpcsFile->addError(
+                sprintf(
+                    'Controller public function name(%s) must end in *Action ', $name
+                ),
+                $openTagPointer,
+                'SymfonyControllerPublicMethodName'
+            );
+        }
+    }
+
+}
